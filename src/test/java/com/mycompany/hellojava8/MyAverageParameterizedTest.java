01c2689d348b6653f991981abdb1394eb6eadef9
dev-di <admin@masterin.se>
2017-08-16 14:25:30 +0200

    test with parameters

--- src/test/java/com/mycompany/hellojava8/MyAverageParameterizedTest.java
+++ src/test/java/com/mycompany/hellojava8/MyAverageParameterizedTest.java
@@ -7,13 +7,43 @@ import static org.hamcrest.CoreMatchers.is;
 import org.junit.Test;
 import static org.junit.Assert.*;
 import org.junit.Ignore;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
 
 /**
  *
  * @author Supergirl
  */
-public class MyMathTest {
+@RunWith(Parameterized.class)
+public class MyAverageParameterizedTest {
+    
+    private List<Integer> input;
+    private int expected;
+    
+    public MyAverageParameterizedTest(List<Integer> input, int expected) {
+        this.input = input;
+        this.expected = expected;
+    }
+    
+    @Parameters
+    public static Collection<Object[]> data() {
+        return Arrays.asList(new Object[][]{
+            {Arrays.asList(1,2,3), 2},
+            {Arrays.asList(3,2,1), 2},
+            {Arrays.asList(2,1,3), 2},
+            {Arrays.asList(2,2,1), 2},
+        });
+    }
+            
 
+    @Test
+    public void testGetAverage() {
+        System.out.println("getAverage");
+        MyMath instance = new MyMath();
+        assertThat(instance.getAverage(input), is(expected));
+    }
+    
     @Test
     public void testGetMin() {
         System.out.println("getMin");
